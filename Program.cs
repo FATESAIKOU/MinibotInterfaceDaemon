963fec89e975ef84ded4dfca609f4bb0c48d14a9
fatesaikou <qzsecftbhhhh@gmail.com>
2018-05-14 13:53:55 +0800

    Initialize Minibot's interface daemone project

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NEXCOMROBOT.MCAT;
+
+//namespace work
+namespace NEXCOMROBOT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            RobotAgent test_agent = new RobotAgent();
+            // test_agent.SetMode(NexMotion_Define.DEV_TYPE_ETHERCAT);
+            test_agent.Init();
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+            test_agent.EnableGroup(0);
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+            test_agent.DisableGroup(0);
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+            test_agent.ResetGroup(0);
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+
+            // Motion test
+            test_agent.EnableGroup(0);
+            test_agent.WaitStatus(0, 576, 100);
+
+            Pos_T some_pos = new Pos_T();
+            some_pos.pos = new double[]{90.0, 90.0, 90.0, 90.0, 90.0, 90.0};
+            test_agent.SetHomePos(0, some_pos);
+            test_agent.SetVelocity(0, some_pos.pos);
+            
+            Console.WriteLine("Start Homing!");
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+            test_agent.HomeAll(0);
+            test_agent.WaitStatus(0, 576, 100);
+            Console.WriteLine("End Homing!");
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+
+            Console.WriteLine("Start AcsJog!");
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+            test_agent.AcsJog(0, 0, 0, 1000, 20);
+            test_agent.WaitStatus(0, 576, 100);
+            Console.WriteLine("End AcsJog!");
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+
+            Console.WriteLine("Start PcsLine!");
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+            some_pos.pos = new double[]{330, 10, -6, 0, 0, -180};
+            Console.WriteLine(test_agent.PcsLine(0, some_pos, 0));
+            test_agent.WaitStatus(0, 576, 100);
+            Console.WriteLine("End PcsLine!");
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+
+            Console.WriteLine("Start AcsPTP!");
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+            some_pos.pos = new double[]{90, 90, 90, 90, 90, 90};
+            Console.WriteLine(test_agent.AcsPTP(0, some_pos));
+            test_agent.WaitStatus(0, 576, 100);
+            Console.WriteLine("End AcsPTP!");
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+
+            Console.WriteLine("Start PcsPTP!");
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+            some_pos.pos = new double[]{93, 9, 821, 87, -0.1, 106};
+            Console.WriteLine(test_agent.PcsPTP(0, some_pos));
+            test_agent.WaitStatus(0, 576, 100);
+            Console.WriteLine("End PcsPTP!");
+            Console.WriteLine(test_agent.GetGroupStatus(0));
+
+            test_agent.Shutdown();        
+        }
+    }
+}
---
+++ RobotAgent.cs
@@ -0,0 +1,238 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NEXCOMROBOT.MCAT;
+
+//namespace work
+namespace NEXCOMROBOT
+{
+    class RobotAgent
+    {
+        private SystemControl mRobot = new SystemControl();
+        private int OperatingMode = NexMotion_Define.DEV_TYPE_SIMULATION;
+
+        /* State Setting */
+        public void Init()
+        {
+            mRobot.InitDevice(OperatingMode);
+
+            foreach (GroupControl g in mRobot.Group)
+            {
+                if (g.GroupAdapter == null || g.GroupParameters == null)
+                {
+                    Console.WriteLine("Null Group Detected!");
+                }
+            }
+            SysParamUpdate();
+        }
+
+        public void Shutdown()
+        {
+            mRobot.DeviceAdapter.NMC_DeviceShutdown();
+        }
+
+        public void SetMode(int type)
+        {
+            this.OperatingMode = type;
+        }
+
+        public void EnableGroup(int group_id)
+        {
+            mRobot.Group[group_id].GroupAdapter.NMC_GroupEnable();
+        }
+
+        public void DisableGroup(int group_id)
+        {
+            mRobot.Group[group_id].GroupAdapter.NMC_GroupDisable();
+        }
+
+        public void ResetGroup(int group_id)
+        {
+            mRobot.Group[group_id].GroupAdapter.NMC_GroupDisable();
+            mRobot.Group[group_id].GroupAdapter.NMC_GroupResetState();
+        }
+        
+        /* State Getting */
+        public string GetGroupStatus(int group_id)
+        {
+            SysParamUpdate();
+
+            var group_parameters = mRobot.Group[group_id].GroupParameters;
+
+            string json_result = "{";
+            json_result += "\"State\":" + group_parameters.State.ToString() + ",\n";
+            json_result += "\"Status\":" + group_parameters.Status.ToString() + ",\n";
+            json_result += "\"Acs\": [" + string.Join(",", group_parameters.ActAcs) + "],\n";
+            json_result += "\"Pcs\": [" + string.Join(",", group_parameters.ActPcs) + "]\n";
+            json_result += "}";
+
+            return json_result;
+        }
+
+        public void WaitStatus(int group_id, int status_mask, int interval)
+        {
+            GroupControl aim_group = mRobot.Group[group_id];
+
+            int status = 0;
+            do {
+                aim_group.GroupAdapter.NMC_GroupGetStatus(ref status);
+                System.Threading.Thread.Sleep(interval);
+            } while((status ^ status_mask) != 0);
+        }
+
+        /* Param Setting */
+        public void SetHomePos(int group_id, Pos_T home_pos)
+        {
+            int mask = 1 << mRobot.Group[group_id].AxisCount - 1;
+
+            mRobot.Group[group_id].GroupAdapter.NMC_GroupSetHomePos(mask, ref home_pos);
+            SysParamUpdate();
+        }
+
+        public void SetVelocity(int group_id, double[] vel)
+        {
+            int param_num = 0x32;
+            int sub_index = 0;
+
+            GroupControl aim_group = mRobot.Group[group_id];
+            for (int axis_index = 0; axis_index < aim_group.AxisCount; ++ axis_index)
+            {
+                aim_group.GroupAdapter.NMC_GroupAxSetParamF64(
+                    axis_index, param_num, sub_index,
+                    vel[axis_index]
+                );
+            }
+            SysParamUpdate();
+        }
+        
+        /* Moving */
+        public int Halt(int group_id)
+        {
+            return mRobot.Group[group_id].GroupAdapter.NMC_GroupHalt();
+        }
+
+        public int Home(GroupControl aim_group, int axis_index)
+        {
+            int param_num = 0x80;
+            int sub_index = 0;
+            int ret;
+
+            ret = aim_group.GroupAdapter.NMC_GroupAxSetParamI32(
+                axis_index, param_num, sub_index,
+                HomingOpcodes[Math.Min(axis_index, 6)]
+            );
+
+            if (ret != NexMotion_ErrCode.NMCERR_SUCCESS) return ret;
+
+            ret = aim_group.GroupAdapter.NMC_GroupAxesHomeDrive(1 << axis_index);
+
+            return ret;
+        }
+
+        public int HomeAll(int group_id)
+        {
+            int ret = 0;
+
+            GroupControl aim_group = mRobot.Group[group_id];
+            for (int axis_index = 0; axis_index < aim_group.AxisCount; ++ axis_index)
+            {
+                ret = Home(aim_group, axis_index);
+                if (ret != NexMotion_ErrCode.NMCERR_SUCCESS) break;
+            }
+
+            return ret;
+        }
+
+        // Add Line, Circle, Jog for Acs/Pcs interface
+        public int AcsJog(int group_id, int axis_index, int direction, int interval,
+            double max_vel /* ohn... no zero */)
+        {
+            GroupControl aim_group = mRobot.Group[group_id];
+
+            int ret;
+            ret = aim_group.GroupAdapter.NMC_GroupJogAcs(axis_index, direction, ref max_vel);
+            if (ret != NexMotion_ErrCode.NMCERR_SUCCESS) return ret;
+
+            System.Threading.Thread.Sleep(interval);
+            ret = aim_group.GroupAdapter.NMC_GroupHalt();
+            return ret;
+        }
+
+        public int PcsLine(int group_id, Pos_T dest,
+            double max_vel /* ohn... it can be zero this time... */)
+        {
+            GroupControl aim_group = mRobot.Group[group_id];
+            int mask = (1 << aim_group.AxisCount) - 1;
+
+            return aim_group.GroupAdapter.NMC_GroupLine(mask, ref dest, ref max_vel);
+        }
+
+        public int AcsPTP(int group_id, Pos_T dest_acs)
+        {
+            GroupControl aim_group = mRobot.Group[group_id];
+            int mask = (1 << aim_group.AxisCount) - 1;
+
+            return aim_group.GroupAdapter.NMC_GroupPtpAcsAll(mask, ref dest_acs);
+        }
+
+        public int PcsPTP(int group_id, Pos_T dest)
+        {
+            GroupControl aim_group = mRobot.Group[group_id];
+            int mask = (1 << aim_group.AxisCount) - 1;
+
+            return aim_group.GroupAdapter.NMC_GroupPtpCartAll(mask, ref dest);
+        }
+
+        /* Other Utils */
+        private int SysParamUpdate()
+        {
+            NexMotion_GroupAdapter groupAdapter;
+            RobotParameters groupParameters;
+
+            int axisCount;
+
+            #region Group
+            foreach (GroupControl g in mRobot.Group)
+            {
+                groupAdapter    = g.GroupAdapter;
+                groupParameters = g.GroupParameters;
+                axisCount       = g.AxisCount;
+
+                groupAdapter.NMC_GroupGetState(ref groupParameters.State);
+                groupAdapter.NMC_GroupGetStatus(ref groupParameters.Status);
+                groupAdapter.NMC_GroupGetActualPosAcs(ref groupParameters.PosAcs);
+                groupAdapter.NMC_GroupGetActualPosPcs(ref groupParameters.PosPcs);
+
+                // WTF...
+                groupParameters.ActAcs = groupParameters.PosAcs.pos;
+                groupParameters.ActPcs = groupParameters.PosPcs.pos;
+            }
+            #endregion
+            
+            /* TODO: IO Sync? */
+
+            return NexMotion_ErrCode.NMCERR_SUCCESS;
+        }
+
+        /* Decoder */
+        private string[] StateMap = new string[] {
+            "GROUP_DISABLE",
+            "GROUP_STAND_STILL",
+            "GROUP_STOPPED",
+            "GROUP_STOPPING",
+            "GROUP_MOVING",
+            "GROUP_HOMING",
+            "GROUP_ERROR_STOP"
+        };
+
+        private int[] HomingOpcodes = new int[] {
+            20, 20, 22, 20, 22, 20,
+            35 // Default is 35
+        };
+    }
+}
