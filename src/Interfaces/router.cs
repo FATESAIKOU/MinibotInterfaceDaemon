5576fca14b4d917703973417228717fbe7b0f985
fatesaikou <qzsecftbhhhh@gmail.com>
2018-06-08 11:56:45 +0800

    Add return exception while device openup failed

--- src/Interfaces/router.cs
+++ src/Interfaces/router.cs
@@ -17,25 +17,25 @@ namespace DaemonCore
             ret = ether_cat_net.InitRobot();
 
             if (ret != NexMotion_ErrCode.NMCERR_SUCCESS)
-                return ret;
+                throw new System.SystemException("Unable to openup device(" + ret.ToString() + ")!!");
 
             ether_cat_net.InitIOForRobot(2, 0);
 
             return ret;
         }
         
-        public RobotStatus DoRoute(string target, string action, object[] params)
+        public RobotStatus DoRoute(string target, string action, object[] args)
         {
             switch (target)
             {
                 case "Robot":
-                    // RobotController;
-                    return RobotController.Do(action, params);
+                    RobotAgent robot_agent = ether_cat_net.GetRobotAgent(0);
+                    return RobotController.Do(robot_agent, action, args);
                 case "Camera":
                     // CameraController;
                     return null;
                 default:
-                    throw new System.ArgumentException("No Such Action!!", "target");
+                    throw new System.ArgumentException("No Such Target!!", "target");
             }
         }
     }
