aa7c8131331fb8720965eca905ef8d68659acdd5
fatesaikou <qzsecftbhhhh@gmail.com>
2018-06-16 22:26:25 +0800

    Plug web interface

---
+++ interfaces/HttpInterface/Controllers/RobotController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DaemonCore;
+using NEXCOMROBOT;
+
+namespace HttpInterface.Controllers
+{
+    [Route("api/[controller]")]
+    public class RobotController : Controller
+    {
+        // GET api/values
+        [HttpGet("{id}")]
+        public RobotStatusReadable Get(int id)
+        {
+            if (id == 0)
+            {
+                return DaemonVars.router.DoRoute("Robot", "GetStatus", null).DoMap();    
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+    }
+}
--- interfaces/HttpInterface/Program.cs
+++ interfaces/HttpInterface/Program.cs
@@ -3,23 +3,61 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using DaemonCore;
+using DaemonInterface;
 
 namespace HttpInterface
 {
+    public static class DaemonVars
+    {
+        public static Route router = new Route();
+        public static StreamReader stdin_stream = new System.IO.StreamReader(Console.OpenStandardInput());
+        public static StreamWriter stdout_stream = new System.IO.StreamWriter(Console.OpenStandardOutput());
+
+    }
+
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            Console.WriteLine("Start Daemon");
+
+            // Init daemon var
+            DaemonVars.router.Start();
+            DaemonVars.stdout_stream.AutoFlush = true;
+
+            // Start web
+            Console.WriteLine("Start Web");
+            Thread th = new Thread(new ThreadStart(StartWeb));
+            th.Start();
+
+            // Start Stdio
+            Console.WriteLine("Start Stdio");
+            Stdio.StartHandle(DaemonVars.router, DaemonVars.stdin_stream, DaemonVars.stdout_stream);
+
+            // Shutdown web
+            Console.WriteLine("Shutdown web");
+            th.Interrupt();
+
+            // Shutdown system
+            Console.WriteLine("Shutdown system");
+            DaemonVars.router.Shudown();
+            
+            Console.WriteLine("End Web");
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static void StartWeb()
+        {
+            IWebHost web_instance = WebHost.CreateDefaultBuilder(null)
                 .UseStartup<Startup>()
                 .Build();
+            web_instance.Run();                
+        }
     }
 }
