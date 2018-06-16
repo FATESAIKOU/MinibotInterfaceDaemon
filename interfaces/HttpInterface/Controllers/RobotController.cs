1cf20cc2e4e85f19036617184904d30633871574
fatesaikou <qzsecftbhhhh@gmail.com>
2018-06-16 22:45:34 +0800

    Complete http interface

--- interfaces/HttpInterface/Controllers/RobotController.cs
+++ interfaces/HttpInterface/Controllers/RobotController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DaemonCore;
 using NEXCOMROBOT;
+using Newtonsoft.Json;
 
 namespace HttpInterface.Controllers
 {
@@ -27,8 +28,23 @@ namespace HttpInterface.Controllers
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public RobotStatusReadable Put(int id, [FromBody]object[] command)
         {
+            try
+            {
+                string target = (string) command[0];
+                string action = (string) command[1];
+                object[] args = (object[]) command[2];
+
+                DaemonVars.router.DoRoute(target, action, args);
+            }
+            catch (InvalidCastException e)
+            {
+                string raw_command = JsonConvert.SerializeObject(command, Formatting.Indented);
+                Console.WriteLine($"[HttpInterface]: bad command({raw_command}), error({e.Message})");
+            }
+            
+            return DaemonVars.router.DoRoute("Robot", "GetStatus", null).DoMap();
         }
     }
 }
